9b93b6253abbc8306669fab8145b96539b85beb1
Leo Mei <liqiangmei@outlook.com>
2022-11-27 17:49:53 -0800

    updated superNode address

--- src/main/java/project/cs249/src/util/Configs.java
+++ src/main/java/project/cs249/src/util/Configs.java
@@ -1,5 +1,5 @@
 package project.cs249.src.util;
 
 public class Configs {
-    public static final String ADDR_SUPERNODE="34.221.186.6:1099";
+    public static final String ADDR_SUPERNODE="34.217.138.113:1099";
 }
